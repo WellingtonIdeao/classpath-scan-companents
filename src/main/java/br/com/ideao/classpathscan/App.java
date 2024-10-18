a3af1354dec21767fc159833f430134c2c02fa4d
Wellington Ideão <wellington.ideao@gmail.com>
2024-10-17 21:43:57 -0300

    feat: using componentScan with AnnotationConfigApplicationContext

--- src/main/java/br/com/ideao/classpathscan/App.java
+++ src/main/java/br/com/ideao/classpathscan/App.java
@@ -1,16 +1,17 @@
 package br.com.ideao.classpathscan;
 
 
+import br.com.ideao.classpathscan.config.ComponentScanConfig;
 import br.com.ideao.classpathscan.domain.SimpleMovieLister;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class App 
 {
     public static void main( String[] args ) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("app.xml");
-        SimpleMovieLister simpleMovieLister = context.getBean("simpleMovieLister",SimpleMovieLister.class);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+        SimpleMovieLister simpleMovieLister = context.getBean(SimpleMovieLister.class);
         System.out.println(simpleMovieLister);
 
         context.registerShutdownHook();
