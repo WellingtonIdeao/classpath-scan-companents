120d35545082b10546afadedc46aa85b2189a51c
Wellington Ideão <wellington.ideao@gmail.com>
2024-10-17 21:14:45 -0300

    feat: using componentScan annotation with classPathXmlApplicationContext

--- src/main/java/br/com/ideao/classpathscan/App.java
+++ src/main/java/br/com/ideao/classpathscan/App.java
@@ -1,5 +1,6 @@
 package br.com.ideao.classpathscan;
 
+
 import br.com.ideao.classpathscan.domain.SimpleMovieLister;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,7 +10,7 @@ public class App
 {
     public static void main( String[] args ) {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("app.xml");
-        SimpleMovieLister simpleMovieLister = context.getBean("simpleMovieLister", SimpleMovieLister.class);
+        SimpleMovieLister simpleMovieLister = context.getBean("simpleMovieLister",SimpleMovieLister.class);
         System.out.println(simpleMovieLister);
 
         context.registerShutdownHook();
---
+++ src/main/java/br/com/ideao/classpathscan/config/ComponentScanConfig.java
@@ -0,0 +1,9 @@
+package br.com.ideao.classpathscan.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.ideao")
+public class ComponentScanConfig {
+}
